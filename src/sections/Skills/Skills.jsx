cdb3128b71831831f67776f40ce7e05f0ff7af74
Bineeth B C <bineethbcr7007@gmail.com>
2024-08-09 22:20:53 +0530

    skills list completed

--- src/sections/Skills/Skills.jsx
+++ src/sections/Skills/Skills.jsx
@@ -1,8 +1,13 @@
 import styles from './SkillsStyles.module.css'
-import checkMarkIcon from '../../assets/checkmark-dark.svg'
+import checkMarkLight from '../../assets/checkmark-light.svg';
+import checkMarkDark from '../../assets/checkmark-dark.svg';
 import SkillList from '../../common/SkillList'
+import { useTheme } from '../../common/ThemeContext';
 
 function Skills() {
+  const { theme } = useTheme();
+  const checkMarkIcon = theme === 'dark' ? checkMarkDark : checkMarkLight;
+
   return (
     <section id='skills' className={styles.container}>
         <h1 className='sectionTitle'>Skills</h1>
