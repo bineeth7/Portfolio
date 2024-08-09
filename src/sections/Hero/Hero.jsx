e00588f3b89100cb2ee1462f6b0bcadc553003ed
Bineeth B C <bineethbcr7007@gmail.com>
2024-08-09 19:27:08 +0530

    insta,whatsapp,gmail  & new hero icon added

--- src/sections/Hero/Hero.jsx
+++ src/sections/Hero/Hero.jsx
@@ -1,13 +1,17 @@
 import styles from "./HeroStyles.module.css";
-import heroImg from "../../assets/hero-img.png";
+import heroImg from "../../assets/bineethbc.png";
 import sun from '../../assets/sun.svg';
 import moon from '../../assets/moon.svg';
-import twitterLight from "../../assets/twitter-light.svg";
-import twitterDark from "../../assets/twitter-dark.svg";
+import gmailLight from '../../assets/gmail-light.svg'
+import gmailDark from '../../assets/gmail-dark.svg'
+import instagramLight from "../../assets/insta-light.svg";
+import instagramDark from "../../assets/insta-dark.svg";
 import githubLight from "../../assets/github-light.svg";
 import githubDark from "../../assets/github-dark.svg";
 import linkedinLight from "../../assets/linkedin-light.svg";
 import linkedinDark from "../../assets/linkedin-dark.svg";
+import whatsappLight from "../../assets/whatsapp-light.svg"
+import whatsappDark from "../../assets/whatsapp-dark.svg"
 import CV from "../../assets/Resume_Bineeth B C.pdf";
 import { useTheme } from "../../common/ThemeContext";
 
@@ -15,9 +19,11 @@ function Hero() {
   const { theme, toggleTheme } = useTheme();
   
   const themeIcon  = theme === 'light' ? sun : moon;
-  const twitterIcon  = theme === 'light' ? twitterLight : twitterDark;
+  const instagramIcon  = theme === 'light' ? instagramLight : instagramDark;
   const githubIcon  = theme === 'light' ? githubLight : githubDark;
   const linkedinIcon  = theme === 'light' ? linkedinLight : linkedinDark;
+  const gmailIcon = theme === 'light' ? gmailLight : gmailDark;
+  const whatsappIcon = theme === 'light' ? whatsappLight : whatsappDark;
 
   return (
     <section id="hero" className={styles.container}>
@@ -34,14 +40,20 @@ function Hero() {
         <h1>Bineeth B C</h1>
         <h2>Front end developer</h2>
         <span>
-          <a href="https://linkedin.com/" target="blank">
+          <a href="mailto:bineethbc7@gmail.com" target="_blank">
+            <img src={gmailIcon} alt="Gmail icon" />
+          </a>
+          <a href="https://linkedin.com/in/bineethbc/" target="_blank">
             <img src={linkedinIcon} alt="Linkedin icon" />
           </a>
-          <a href="https://github.com/bineeth7" target="blank">
+          <a href="https://github.com/bineeth7" target="_blank">
             <img src={githubIcon} alt="Github icon" />
           </a>
-          <a href="https://twitter.com/" target="blank">
-            <img src={twitterIcon} alt="Twitter icon" />
+          <a href="https://wa.me/9605354673" target="_blank">
+            <img src={whatsappIcon} alt="Whatsapp icon" />
+          </a>
+          <a href="https://www.instagram.com/invic_7us?igsh=MWJvZm56MzJ4aWF3Ng==" target="_blank">
+            <img src={instagramIcon} alt="Instagram icon" />
           </a>
         </span>
         <p className={styles.description}>
